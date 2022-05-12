9673c080fe5692b4c33ac5ba5504e7cd9eca787f
jasonzhou1997 <60407587+jasonzhou1997@users.noreply.github.com>
2022-05-12 10:25:01 -0400

    Need2implement const [] operator

--- Array.cpp
+++ Array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 using namespace std;
 
 class Array {
@@ -10,6 +11,38 @@ public:
     length{len},
     data{new double[length]} {}
 
+    // copy constructor:
+    Array(const Array &a):
+    length{a.length},
+    data{new double[length]}
+    {
+        for (size_t i = 0; i < length; i++)
+        {
+            data[i] = a.data[i];
+        }
+    }
+    //copy swap method:
+    Array& operator=(const Array &rhs)
+    {
+        Array temp(rhs);
+
+        swap(length, temp.length);
+        swap(data, temp.data);
+
+        return *this;
+    }
+
+    // operator[] overloading
+    double &operator[](size_t i)
+    {
+        if (i < length)
+        {
+            return data[i];
+        }
+        throw runtime_error("bad index i");
+    }
+    // need to also implement a const version of operator[]:
+
     ~Array(){
         delete[] data;
         data = nullptr;
@@ -20,7 +53,38 @@ public:
     }
 };
 
+// print the array
+ostream &operator<<(ostream &os, const Array &Arr)
+{
+    for (size_t i = 0; i < Arr.size(); i++)
+    {
+        os << Arr[i] << " ";
+    }
+    return os;
+}
+
 int main()
 {
+    cout << "Test Array: \n";
+    Array a(10);
+    Array b(20);
+    Array c{b};
+    cout << "size of a: " << a.size() << "\n";
+    cout << "size of b: " << b.size() << "\n";
+    cout << "size of c: " << c.size() << "\n";
+
+    Array d = b;
+    cout << "size of c: " << d.size() << "\n";
+    // shallow copy, resulting double free error with the destructor
+    a = c;
+    cout << "size of a now: " << a.size() << "\n";
+
+    cout << "More Test: \n";
+    Array A(3);
+    A[0] = 25;
+    A[2] = 30;
+    A[1] = A[0] + A[2];
+
+
     return 0;
 }
