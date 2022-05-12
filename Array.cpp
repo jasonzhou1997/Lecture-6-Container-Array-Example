29ad66bb9b67b502010e68f7ae5a82defb810304
jasonzhou1997 <60407587+jasonzhou1997@users.noreply.github.com>
2022-05-12 12:51:39 -0400

    Done with insertion and const << operator

--- Array.cpp
+++ Array.cpp
@@ -2,27 +2,26 @@
 #include <utility>
 using namespace std;
 
-class Array {
+class Array
+{
     size_t length = 0;
     double *data = nullptr;
 
 public:
-    Array(size_t len):
-    length{len},
-    data{new double[length]} {}
+    Array(size_t len) : length{len},
+                        data{new double[length]} {}
 
     // copy constructor:
-    Array(const Array &a):
-    length{a.length},
-    data{new double[length]}
+    Array(const Array &a) : length{a.length},
+                            data{new double[length]}
     {
         for (size_t i = 0; i < length; i++)
         {
             data[i] = a.data[i];
         }
     }
-    //copy swap method:
-    Array& operator=(const Array &rhs)
+    // copy swap method:
+    Array &operator=(const Array &rhs)
     {
         Array temp(rhs);
 
@@ -42,18 +41,49 @@ public:
         throw runtime_error("bad index i");
     }
     // need to also implement a const version of operator[]:
+    const double &operator[](size_t i) const
+    {
+        if (i < length)
+        {
+            return data[i];
+        }
+        throw runtime_error("bad index i");
+    }
 
-    ~Array(){
+    ~Array()
+    {
         delete[] data;
         data = nullptr;
     }
 
-    size_t size() const{
+    size_t size() const
+    {
         return length;
     }
+
+    // Insert
+    bool insert(size_t index, double val)
+    {
+        if (index >= length)
+        {
+            return false;
+        }
+        // Copy Data
+        for (size_t i = length - 1; i > index; --i)
+        {
+            data[i] = data[i - 1];
+        }
+        // Overwrite old with new
+        data[index] = val;
+        return true;
+    }
+    friend ostream &operator<<(ostream &os,
+                               const Array &Arr);
 };
 
 // print the array
+// since first parameter is an ostream, the operator can not be a member
+// function of your user-defined class
 ostream &operator<<(ostream &os, const Array &Arr)
 {
     for (size_t i = 0; i < Arr.size(); i++)
@@ -84,7 +114,9 @@ int main()
     A[0] = 25;
     A[2] = 30;
     A[1] = A[0] + A[2];
-
+    cout << A[0] << " " << A[1] << " " << A[2] << "\n";
+    A.insert(1, 100);
+    cout << A[0] << " " << A[1] << " " << A[2] << "\n";
 
     return 0;
 }
